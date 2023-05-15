772535d2e014b21fa1d775a88c0ac625ccc4312e
bhavs222 <parnasalabhavana222@gmail.com>
2023-05-15 20:03:46 +0530

    upd

--- bhgit.py
+++ bhgit.py
@@ -1,1 +1,7 @@
-print("Welcome to EmployeeWage ComputationProgram on Main branch")
+import random 
+print("Welcome to EmployeeWage  ComputationProgram on Main branch")
+randnumber=random.randint(0,1)
+if(randnumber==0):
+    print("employee is present")
+else:
+    print("employee is absent")        
