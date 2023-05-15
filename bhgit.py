12f75be42ea0f1b64b1fcbc9584c6b8b3f953414
bhavs222 <parnasalabhavana222@gmail.com>
2023-05-15 20:23:28 +0530

    updted

--- bhgit.py
+++ bhgit.py
@@ -1,7 +1,11 @@
 import random 
 print("Welcome to EmployeeWage  ComputationProgram on Main branch")
 randnumber=random.randint(0,1)
+wagePerhour=20
+fulldayhour=8
 if(randnumber==0):
     print("employee is present")
+    dailywage=wagePerhour*fulldayhour
+    print(dailywage)
 else:
     print("employee is absent")        
